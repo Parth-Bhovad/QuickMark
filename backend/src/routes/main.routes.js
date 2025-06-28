b0d2ecfc6d23665ddfabd836a34164b10ae24b96
Parth Bhovad <parthbhovad710@gmail.com>
2025-06-28 15:32:29 +0530

    changed the name of all user components to student

--- backend/src/routes/main.routes.js
+++ backend/src/routes/main.routes.js
@@ -2,7 +2,7 @@ import {Router} from 'express';
 
 //importing individual routers
 import authenticateRouter from './authenticate.routes.js';
-import userRouter from './user.routes.js';
+import studentRouter from './student.routes.js';
 import subjectRouter from './subject.routes.js';
 import attendanceRouter from './attendance.routes.js';
 import teacherRouter from './teacher.routes.js';
@@ -10,7 +10,7 @@ import teacherRouter from './teacher.routes.js';
 const mainRouter = Router({ mergeParams: true });
 
 mainRouter.use('/authenticate', authenticateRouter);
-mainRouter.use('/users', userRouter);
+mainRouter.use('/students', studentRouter);
 mainRouter.use('/subjects', subjectRouter);
 mainRouter.use('/attendance', attendanceRouter);
 mainRouter.use('/teachers', teacherRouter);
