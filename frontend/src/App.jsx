81bf34554306531c5246ba1315af01c34afcbe72
Parth Bhovad <parthbhovad710@gmail.com>
2025-06-28 20:59:21 +0530

    Basic Page created and connected via routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
-
+import Navbar from './components/Navbar'
+import LandingPage from './pages/LandingPage'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import AttendancePage from './pages/AttendancePage'
+import AttendanceList from './pages/AttendanceList'
+import { BrowserRouter, Routes, Route } from "react-router";
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/attendance" element={<AttendancePage />} />
+        <Route path="/attendance-list" element={<AttendanceList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/AttendancePage.jsx
@@ -0,0 +1,9 @@
+function AttendancePage() {
+    return ( 
+        <div>
+            <h1>Attendance Page</h1>
+        </div>
+    );
+}
+
+export default AttendancePage;
