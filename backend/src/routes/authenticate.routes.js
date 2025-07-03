6912b372a8a02a285bb2211332bdffb86299ebde
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-03 19:12:23 +0530

    wrapAsync function implemented and middleware added to validate inputs

--- backend/src/routes/authenticate.routes.js
+++ backend/src/routes/authenticate.routes.js
@@ -2,15 +2,22 @@ import { Router } from 'express';
 
 //importing controllers
 import { sendOTPEmail, verifyOTPEmail } from '../controllers/auth.controller.js';
+//importing middlewares
+import validateRequest from '../middlewares/validateRequest.middleware.js';
+//import Joi schemas
+import { emailValidator } from '../validators/email.validator.js';
+import { otpValidator } from '../validators/otp.validator.js';
+//importing wrapAsync
+import wrapAsync from '../utils/wrapAsync.js';
 
 const authenticateRouter = Router({ mergeParams: true });
 
-authenticateRouter.post('/send-otp', sendOTPEmail);
+authenticateRouter.post('/send-otp', validateRequest(emailValidator), wrapAsync(sendOTPEmail));
 
-authenticateRouter.post('/resend-otp', (req, res) => {
+authenticateRouter.post('/resend-otp', wrapAsync((req, res) => {
     console.log("resend-otp route");
-});
+}));
 
-authenticateRouter.post('/verify-otp', verifyOTPEmail);
+authenticateRouter.post('/verify-otp', validateRequest(otpValidator), wrapAsync(verifyOTPEmail));
 
 export default authenticateRouter;
