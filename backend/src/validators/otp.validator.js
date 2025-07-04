fb70cf4a68c67e014cc4c727a70ee38c92cbe773
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-04 10:21:34 +0530

    validation schema changed

--- backend/src/validators/otp.validator.js
+++ backend/src/validators/otp.validator.js
@@ -4,5 +4,12 @@ export const otpValidator = Joi.object({
     otp: Joi.string().length(6).required().messages({
         'string.length': 'OTP must be 6 characters long',
         'any.required': 'OTP is required',
-    })
+    }),
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.email': 'Invalid email format',
+            'any.required': 'Email is required',
+        })
 });
