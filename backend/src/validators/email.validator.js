fb73f1d38e6f11e46d40748c739a4346de2b016c
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-04 10:21:46 +0530

    validation schema changed

--- backend/src/validators/email.validator.js
+++ backend/src/validators/email.validator.js
@@ -1,9 +1,11 @@
 import Joi from 'joi';
 
-export const emailValidator = Joi.string()
-    .email()
-    .required()
-    .messages({
-        'string.email': 'Invalid email format',
-        'any.required': 'Email is required',
-    });
+export const emailValidator = Joi.object({
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.email': 'Invalid email format',
+            'any.required': 'Email is required',
+        })
+});
