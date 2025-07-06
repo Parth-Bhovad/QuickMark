9952c4db7280b7542555dd11587afe79a702fdf9
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-06 12:54:59 +0530

    authMe controller added to check user is logged in or not in frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 //import necessary services
 import { registerStudentService, loginStudentService, registerTeacherService, loginTeacherService, sendOTPEmailService, verifyOTPService } from '../services/auth.service.js';
+import ExpressError from '../utils/ExpressError.js';
+import jwt from 'jsonwebtoken';
 
 export const registerStudent = async (req, res) => {
   let { studentName, rollNo, studentEmail, studentPassword } = req.body;
@@ -11,44 +13,64 @@ export const registerStudent = async (req, res) => {
 }
 
 export const loginStudent = async (req, res) => {
-    let { studentEmail, studentPassword } = req.body;
-    const { token } = await loginStudentService(studentEmail, studentPassword);
-    res.cookie('token', token, {
-      httpOnly: true,
-    });
-    res.status(200).json({ token });
+  let { studentEmail, studentPassword } = req.body;
+  const { token } = await loginStudentService(studentEmail, studentPassword);
+  res.cookie('token', token, {
+    httpOnly: true,
+  });
+  res.status(200).json({ token });
 }
 
 export const registerTeacher = async (req, res) => {
-    let { teacherName, teacherEmail, teacherPassword } = req.body;
-    const { token } = await registerTeacherService(teacherName, teacherEmail, teacherPassword);
-    res.cookie('token', token, {
-      httpOnly: true,
-    });
-    res.status(201).json({ token });
+  let { teacherName, teacherEmail, teacherPassword } = req.body;
+  const { token } = await registerTeacherService(teacherName, teacherEmail, teacherPassword);
+  res.cookie('token', token, {
+    httpOnly: true,
+  });
+  res.status(201).json({ token });
 }
 
 export const loginTeacher = async (req, res) => {
-    let { teacherEmail, teacherPassword } = req.body;
-    const { token } = await loginTeacherService(teacherEmail, teacherPassword);
-    res.cookie('token', token, {
-      httpOnly: true,
-    });
-    res.status(200).json({ token });
+  let { teacherEmail, teacherPassword } = req.body;
+  const { token } = await loginTeacherService(teacherEmail, teacherPassword);
+  res.cookie('token', token, {
+    httpOnly: true,
+  });
+  res.status(200).json({ token });
 }
 
 export const sendOTPEmail = async (req, res) => {
-    const { email } = req.body;
-    const otp = await sendOTPEmailService(email);
-    res.status(200).json({ message: 'OTP sent successfully' });
+  const { email } = req.body;
+  const otp = await sendOTPEmailService(email);
+  res.status(200).json({ message: 'OTP sent successfully' });
 };
 
 export const verifyOTPEmail = async (req, res) => {
-    const { email, otp } = req.body;
-    const isValid = await verifyOTPService(email, otp);
-    if (isValid) {
-      res.status(200).json({ message: 'OTP verified successfully' });
-    } else {
-      res.status(400).json({ error: 'Invalid OTP' });
-    }
+  const { email, otp } = req.body;
+  const isValid = await verifyOTPService(email, otp);
+  if (isValid) {
+    res.status(200).json({ message: 'OTP verified successfully' });
+  } else {
+    res.status(400).json({ error: 'Invalid OTP' });
+  }
+};
+
+export const authMe = async (req, res) => {
+  console.log("authMe called");
+
+  const token = req.cookies.token;
+  console.log("Token from cookies:", token);
+
+  if (!token) {
+    console.log("No token found, returning unauthorized error");
+
+    throw new ExpressError(401, "Unauthorized");
+  }
+  console.log("1");
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!decoded) throw new ExpressError(401, "Unauthorized");
+  console.log("2");
+  res.status(200).json({ user: decoded });
 };
