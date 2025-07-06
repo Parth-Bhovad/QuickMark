04f451e2fe145b46f69dac47ed0412542741713a
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-06 13:13:40 +0530

    Authorization route implemented in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,28 @@ import TeacherLoginPage from './features/teacher/pages/TeacherLoginPage'
 import TeacherSignupPage from './features/teacher/pages/TeacherSignupPage'
 import AttendancePage from './pages/AttendancePage'
 import AttendanceList from './pages/AttendanceList'
-import TeacherAttendanceSession from './pages/TeacherAttendanceSession'
-import StudentMarkAttendance from './pages/StudentMarkAttendance'
-import TeacherProfile from './pages/TeacherProfile'
-import StudentProfile from './pages/StudentProfile'
 import { BrowserRouter, Routes, Route } from "react-router";
+import StudentRoute from './routes/StudentRoute';
+import TeacherRoute from './routes/TeacherRoute';
+import { AuthProvider } from './context/AuthContext';
 function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<StudentLoginPage />} />
-        <Route path="/signup" element={<StudentSignupPage />} />
-        <Route path="/teacher-login" element={<TeacherLoginPage />} />
-        <Route path="/teacher-signup" element={<TeacherSignupPage />} />
-        <Route path="/attendance" element={<AttendancePage />} />
-        <Route path="/attendance-list" element={<AttendanceList />} />
-        <Route path="/teacher-attendance-session" element={<TeacherAttendanceSession />} />
-        <Route path="/student-mark-attendance" element={<StudentMarkAttendance />} />
-        <Route path="/teacher-profile" element={<TeacherProfile />} />
-        <Route path="/student-profile" element={<StudentProfile />} />
-      </Routes>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/student-login" element={<StudentLoginPage />} />
+          <Route path="/student-signup" element={<StudentSignupPage />} />
+          <Route path="/teacher-login" element={<TeacherLoginPage />} />
+          <Route path="/teacher-signup" element={<TeacherSignupPage />} />
+          <Route path="/attendance" element={<AttendancePage />} />
+          <Route path="/attendance-list" element={<AttendanceList />} />
+          <Route path="/student/*" element={<StudentRoute />} />
+          <Route path="/teacher/*" element={<TeacherRoute />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
