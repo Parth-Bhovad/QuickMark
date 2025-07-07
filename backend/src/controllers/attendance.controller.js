b1fcb0c62be99896891cc3eed2c2066392c081e1
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-07 18:59:49 +0530

    getting otp as key instead of subjectName

--- backend/src/controllers/attendance.controller.js
+++ backend/src/controllers/attendance.controller.js
@@ -2,16 +2,19 @@
 import { addAttendanceService } from "../services/attendance.service.js";
 //importing utils
 import { getVerificationCode } from "../utils/generateOTP.js";
+import { findSubjectByName } from "../dao/subject.dao.js";
 
 export const addAttendance = async (req, res) => {
     try {
-        const getOTP = getVerificationCode(req.body.subjectName);
-        console.log(getOTP);
-        
-        if (getOTP != req.body.otp) {
+        const getSubjectName = getVerificationCode(req.body.otp);
+        console.log(getSubjectName);
+        // If the OTP is invalid, getVerificationCode will return undefined
+        // If the OTP is valid, it will return the subject name associated with that OTP    
+        if (getSubjectName=== undefined) {
             return res.status(404).json({ message: "Invalid OTP" });
         }
-        const {studentId, subjectId, isPresent=false} = req.body;
+        const subjectId = await findSubjectByName(getSubjectName);
+        const {studentId, isPresent=false} = req.body;
         const attendance = await addAttendanceService({studentId, subjectId, isPresent});
         res.status(201).json({ message: "Attendance added successfully", attendance });
     } catch (error) {
