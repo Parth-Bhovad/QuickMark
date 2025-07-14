81303131dd5e4c2b71fa812bb6afe82f0161b89f
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-14 11:50:45 +0530

    UI improved and form validation with feedback msg added

--- frontend/src/features/student/components/StudentLogin.jsx
+++ frontend/src/features/student/components/StudentLogin.jsx
@@ -1,34 +1,57 @@
-import InputField from "../../../components/InputField";
-
-//importing custom hook
+import { Form, Button } from "react-bootstrap";
 import useAuth from "../hooks/useAuth";
 
 function StudentLogin() {
-    //using custom hook
-    const { studentEmail, setStudentEmail, studentPassword, setStudentPassword, handleLoginStudent } = useAuth();
-    return (
-        <>
-            <h1>Login Page</h1>
-            <form onSubmit={handleLoginStudent}>
-                <InputField
-                    label={"Student Email:"}
-                    id={"email"}
-                    name={"studentEmail"}
-                    required={true}
-                    value={studentEmail}
-                    onChange={(e) => setStudentEmail(e.target.value)} />
-                <InputField
-                    label={"Student Password:"}
-                    id={"password"}
-                    name={"studentPassword"}
-                    required={true}
-                    type={"password"}
-                    value={studentPassword}
-                    onChange={(e) => setStudentPassword(e.target.value)} />
-                <button type="submit">Login</button>
-            </form>
-        </>
-    );
+  // using custom hook
+  const {
+    studentEmail,
+    setStudentEmail,
+    studentPassword,
+    setStudentPassword,
+    handleLoginStudent,
+    validated,
+  } = useAuth();
+
+  return (
+    <main className="container">
+      <h3 className="text-center mb-5 mt-3">Student Login Page</h3>
+
+      <Form noValidate validated={validated} onSubmit={handleLoginStudent}>
+        <Form.Group className="mb-3" controlId="formStudentEmail">
+          <Form.Label>Student Email:</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            value={studentEmail}
+            onChange={(e) => setStudentEmail(e.target.value)}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-4" controlId="formStudentPassword">
+          <Form.Label>Student Password:</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            minLength={6}
+            placeholder="Enter password"
+            value={studentPassword}
+            onChange={(e) => setStudentPassword(e.target.value)}
+          />
+          <Form.Control.Feedback type="invalid">
+            Password should be at least 6 characters.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Button type="submit" className="w-100">
+          Login
+        </Button>
+      </Form>
+    </main>
+  );
 }
 
 export default StudentLogin;
