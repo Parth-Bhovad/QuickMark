3b7ffa47e95469fd08c0bf6a4da12971383e4000
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-14 11:52:25 +0530

    UI improved and form validation with feedback msg added

--- frontend/src/features/teacher/components/TeacherLogin.jsx
+++ frontend/src/features/teacher/components/TeacherLogin.jsx
@@ -1,40 +1,57 @@
-import InputField from "../../../components/InputField";
-//importing custom hooks
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import useAuth from "../hooks/useAuth";
 
 function TeacherLogin() {
-    const {
-        teacherEmail,
-        setTeacherEmail,
-        teacherPassword,
-        setTeacherPassword,
-        handleLoginTeacher
-    } = useAuth();
+  const {
+    teacherEmail,
+    setTeacherEmail,
+    teacherPassword,
+    setTeacherPassword,
+    handleLoginTeacher,
+    validated
+  } = useAuth();
 
-    return (
-        <>
-            <h1>Teacher Login Page</h1>
-            <form>
-                <InputField
-                    label={"Teacher Email:"}
-                    id={"email"}
-                    name={"teacherEmail"}
-                    required={true}
-                    value={teacherEmail}
-                    onChange={(e) => setTeacherEmail(e.target.value)} />
-                    
-                <InputField
-                    label={"Teacher Password:"}
-                    id={"password"}
-                    name={"teacherPassword"}
-                    required={true}
-                    type={"password"}
-                    value={teacherPassword}
-                    onChange={(e) => setTeacherPassword(e.target.value)} />
-                <button type="submit" onClick={handleLoginTeacher}>Login</button>
-            </form>
-        </>
-    );
+  return (
+    <main className="container">
+      <h3 className="text-center mb-5 mt-3">Teacher Login Page</h3>
+
+      <Form noValidate validated={validated} onSubmit={handleLoginTeacher}>
+        <Form.Group className="mb-3" controlId="formTeacherEmail">
+          <Form.Label>Teacher Email:</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            value={teacherEmail}
+            onChange={(e) => setTeacherEmail(e.target.value)}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-4" controlId="formTeacherPassword">
+          <Form.Label>Teacher Password:</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            minLength={6}
+            placeholder="Enter password"
+            value={teacherPassword}
+            onChange={(e) => setTeacherPassword(e.target.value)}
+          />
+          <Form.Control.Feedback type="invalid">
+            Password should be at least 6 characters.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Button type="submit" className="w-100">
+          Login
+        </Button>
+      </Form>
+    </main>
+  );
 }
 
 export default TeacherLogin;
