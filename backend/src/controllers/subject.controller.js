f213101c59e56e7e544ad89c0b446c382f3b67db
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-19 16:49:53 +0530

    getAvailableSubjects controller added and try catch blocks removed

--- backend/src/controllers/subject.controller.js
+++ backend/src/controllers/subject.controller.js
@@ -1,12 +1,13 @@
 //importing services
-import { createSubjectService } from '../services/subject.service.js';
+import { createSubjectService, getAvailableSubjectsService } from '../services/subject.service.js';
 
 export const createSubject = async (req, res) => {
     const { subjectName, teacherId } = req.body;
-    try {
         const newSubject = await createSubjectService(subjectName, teacherId);
         res.status(201).json(newSubject);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
 }
+
+export const getAvailableSubjects = async (req, res) => {
+        const availableSubjects = await getAvailableSubjectsService();
+        res.status(200).json({ availableSubjects });
+};
