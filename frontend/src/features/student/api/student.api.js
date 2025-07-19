86d6ff07be47832545aac841f00d891b927f3884
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-19 12:04:41 +0530

    addSubjectToStudentAPI and getStudentProfileAPI added

--- frontend/src/features/student/api/student.api.js
+++ frontend/src/features/student/api/student.api.js
@@ -13,3 +13,13 @@ export const checkActiveSessionAPI = async () => {
     const response = await api.get("attendance/check-active-session");
     return response;
 }
+
+export const addSubjectToStudentAPI = async (studentId, subjectName) => {
+    const response = await api.patch(`/students/${studentId}/subjects`, { subjectName });
+    return response.data;
+}
+
+export const getStudentProfileAPI = async (studentId) => {
+    const response = await api.get(`/students/${studentId}`);
+    return response.data;
+}
