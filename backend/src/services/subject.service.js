b6c56dbef81114e50d6b9fa48e850b87b486e342
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-19 16:56:20 +0530

    getAvailableSubjectsService service added and error handling added

--- backend/src/services/subject.service.js
+++ backend/src/services/subject.service.js
@@ -1,7 +1,21 @@
 //importing DAOs
-import { createSubject } from '../dao/subject.dao.js';
+import { createSubject,getAvailableSubjects } from '../dao/subject.dao.js';
+//importing Express Error Handler
+import ExpressError from '../utils/ExpressError.js';
 
 export const createSubjectService = async (subjectName, teacherId) => {
+    const existingSubject = await findSubjectByName(subjectName);
+    if (existingSubject) {
+        throw new ExpressError(400, "Subject already exists");
+    }
     const newSubject = await createSubject(subjectName, teacherId);
     return newSubject;
+}
+
+export const getAvailableSubjectsService = async () => {
+    const availableSubjects = await getAvailableSubjects();
+    if (availableSubjects.length > 0) {
+        return availableSubjects;
+    }
+    throw new ExpressError(404, "No available subjects found");
 }
