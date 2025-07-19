a05ca5a7d82f05d43e286c1f48e2bc3a2ff70306
Parth Bhovad <parthbhovad710@gmail.com>
2025-07-19 16:56:46 +0530

    Error handling added

--- backend/src/services/teacher.service.js
+++ backend/src/services/teacher.service.js
@@ -1,11 +1,13 @@
 import { findTeacherById, deleteTeacherById, addSubjectToTeacher } from '../dao/teacher.dao.js';
 import { findSubjectByName, findSubjectById } from '../dao/subject.dao.js';
 import { createSubject } from '../dao/subject.dao.js';
+//import Express Error
+import ExpressError from '../utils/ExpressError.js';
 
 export const getTeacherService = async (teacherId) => {
     const teacher = await findTeacherById(teacherId);
     if (!teacher) {
-        throw new Error(`Teacher with ID: ${teacherId} not found`);
+        throw new ExpressError(404, `Teacher with ID: ${teacherId} not found`);
     }
     const subjectsNameArray = await getTeacherSubjectsService(teacherId);
     return { teacherName: teacher.teacherName, subjects: subjectsNameArray };
@@ -14,17 +16,21 @@ export const getTeacherService = async (teacherId) => {
 export const deleteTeacherService = async (teacherId) => {
     const teacher = await findTeacherById(teacherId);
     if (!teacher) {
-        throw new Error(`Teacher with ID: ${teacherId} not found`);
+        throw new ExpressError(404, `Teacher with ID: ${teacherId} not found`);
     }
     await deleteTeacherById(teacherId);
     return teacher;
 }
 
 export const addSubjectToTeacherService = async (subjectName, teacherId) => {
+    const subject = await findSubjectByName(subjectName);
     //check if the subject already exists
-    let subject = await findSubjectByName(subjectName);
     if (!subject) {
         subject = await createSubject(subjectName, teacherId);
+    }
+    const teacher = await findTeacherById(teacherId);
+    if (teacher.subjects && teacher.subjects.includes(subject._id)) {
+        throw new ExpressError(400, `Subject ${subjectName} already exists for teacher`);
     }
 
     await addSubjectToTeacher(teacherId, subject._id);
@@ -34,13 +40,20 @@ export const addSubjectToTeacherService = async (subjectName, teacherId) => {
 export const getTeacherSubjectsService = async (teacherId) => {
     const teacher = await findTeacherById(teacherId);
     if (!teacher) {
-        throw new Error(404, `Teacher with ID: ${teacherId} not found`);
+        throw new ExpressError(404, `Teacher with ID: ${teacherId} not found`);
     }
-    const subjectsNameArray=[];
-    for (let i = 0; i < teacher.subjects.length; i++) {
-        const subjectId = teacher.subjects[i];
-        const subjectName = await findSubjectById(subjectId);
-        subjectsNameArray.push(subjectName);
+    const subjectsNameArray = [];
+    if (teacher?.subjects && teacher?.subjects?.length > 0) {
+        for (let i = 0; i < teacher.subjects.length; i++) {
+            const subjectId = teacher.subjects[i];
+            const subjectName = await findSubjectById(subjectId);
+            subjectsNameArray.push(subjectName);
+        }
     }
+
+    if (!subjectsNameArray || subjectsNameArray.length === 0) {
+        throw new ExpressError(404, `Subjects not found`);
+    }
+
     return subjectsNameArray;
 }
