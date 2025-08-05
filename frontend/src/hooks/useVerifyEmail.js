796477978756d1283089a33e6a653ef2bf4f67a6
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-05 08:41:18 +0530

    emailVerificationError state added to show errors related to email verifications

--- frontend/src/hooks/useVerifyEmail.js
+++ frontend/src/hooks/useVerifyEmail.js
@@ -1,38 +1,54 @@
 import { useState } from "react";
 //importing APIs
-import {sendOTPToEmailAPI, verifyOtpAPI} from "../api/verifyEmail.api";
+import { sendOTPToEmailAPI, verifyOtpAPI } from "../api/verifyEmail.api";
 
 function useVerifyEmail() {
-    const [isOTPSent, setIsOTPSent] = useState(false);
-    const [isOTPVerified, setIsOTPVerified] = useState(false);
-    const [otp, setOtp] = useState("");
+  const [isOTPSent, setIsOTPSent] = useState(false);
+  const [isOTPVerified, setIsOTPVerified] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [emailVerificationError, setEmailVerificationError] = useState("");
 
-    const handleSendOTPToEmail = async (email) => {
-        const response = await sendOTPToEmailAPI(email);
-        console.log(response);
-        
-        if (response.status === 200) {
-            setIsOTPSent(true);
-        }
-    };
+  const handleSendOTPToEmail = async (email) => {
+    try {
+      const response = await sendOTPToEmailAPI(email);
+      console.log(response);
 
-    const handleVerifyOtp = async (email) => {
-        const response = await verifyOtpAPI(email, otp);
-        console.log(response);
+      if (response.status === 200) {
+        setIsOTPSent(true);
+      }
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+      setEmailVerificationError(
+        error.response.data.msg || error.response.data.error
+      );
+    }
+  };
 
-        if (response.status === 200) {
-            setIsOTPVerified(true);
-        }
-    };
+  const handleVerifyOtp = async (email) => {
+    try {
+      const response = await verifyOtpAPI(email, otp);
+      console.log(response);
 
-    return {
-        isOTPSent,
-        isOTPVerified,
-        otp,
-        setOtp,
-        handleSendOTPToEmail,
-        handleVerifyOtp
-    };
+      if (response.status === 200) {
+        setIsOTPVerified(true);
+      }
+    } catch (error) {
+      console.error("Error verifying OTP:", error);
+      setEmailVerificationError(
+        error.response.data.msg || error.response.data.error
+      );
+    }
+  };
+
+  return {
+    isOTPSent,
+    isOTPVerified,
+    otp,
+    setOtp,
+    handleSendOTPToEmail,
+    handleVerifyOtp,
+    emailVerificationError,
+  };
 }
 
 export default useVerifyEmail;
