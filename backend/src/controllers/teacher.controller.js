7b635cfbc5faf0a11cedb588a5dd339387ec9893
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-05 08:34:42 +0530

    Try catch block removed

--- backend/src/controllers/teacher.controller.js
+++ backend/src/controllers/teacher.controller.js
@@ -16,15 +16,10 @@ export const deleteTeacher = async (req, res) => {
 };
 
 export const addSubjectToTeacher = async (req, res) => {
-    try {
         const { subjectName } = req.body;
         const teacherId = req.params.teacherId;
         await addSubjectToTeacherService(subjectName, teacherId);
         res.status(200).json({ message: "Subject added to teacher successfully" });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-        
-    }
 }
 
 export const getTeacherSubjects = async (req, res) => {
