8fa7f29af55a860d844eed5702c845b263bdbd66
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-08 16:52:38 +0530

    blocking screen in certain range to prevent split screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import LandingPage from './pages/LandingPage'
 import StudentLoginPage from './features/student/pages/StudentLoginPage'
@@ -13,24 +14,45 @@ import { Routes, Route } from "react-router";
 import StudentRoute from './routes/StudentRoute';
 import TeacherRoute from './routes/TeacherRoute';
 function App() {
+  useEffect(() => {
+    const checkSplit = () => {
+      const blocker = document.getElementById("splitScreenBlocker");
+      const w = window.innerWidth;
+      const h = window.innerHeight;
+
+      if (w <= 300 || h <= 600) {
+        blocker.style.display = "flex";
+      } else {
+        blocker.style.display = "none";
+      }
+    };
+
+    checkSplit();
+    window.addEventListener("resize", checkSplit);
+    return () => window.removeEventListener("resize", checkSplit);
+  }, []);
 
   return (
-        <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/student-login" element={<StudentLoginPage />} />
-          <Route path="/student-forgot-password" element={<StudentForgotPasswordPage />} />
-          <Route path="/student-signup" element={<StudentSignupPage />} />
-          <Route path="/teacher-login" element={<TeacherLoginPage />} />
-          <Route path="/teacher-forgot-password" element={<TeacherForgotPasswordPage />} />
-          <Route path="/teacher-signup" element={<TeacherSignupPage />} />
-          <Route path="/attendance" element={<AttendancePage />} />
-          <Route path="/attendance-list" element={<AttendanceList />} />
-          <Route path="/student/*" element={<StudentRoute />} />
-          <Route path="/teacher/*" element={<TeacherRoute />} />
-        </Routes>
-      </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/student-login" element={<StudentLoginPage />} />
+        <Route path="/student-forgot-password" element={<StudentForgotPasswordPage />} />
+        <Route path="/student-signup" element={<StudentSignupPage />} />
+        <Route path="/teacher-login" element={<TeacherLoginPage />} />
+        <Route path="/teacher-forgot-password" element={<TeacherForgotPasswordPage />} />
+        <Route path="/teacher-signup" element={<TeacherSignupPage />} />
+        <Route path="/attendance" element={<AttendancePage />} />
+        <Route path="/attendance-list" element={<AttendanceList />} />
+        <Route path="/student/*" element={<StudentRoute />} />
+        <Route path="/teacher/*" element={<TeacherRoute />} />
+      </Routes>
+      {/* Split-screen blocker overlay */}
+      <div id="splitScreenBlocker" className="split-blocker">
+        <p>Please expand your view to full screen to mark attendance.</p>
+      </div>
+    </>
   );
 }
 
