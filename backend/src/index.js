aa90ee1aa0a68048a3b4069cdc8db08b42496160
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-08 18:51:36 +0530

    production link added

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
-    origin: process.env.NODE_ENV === "production" ? "prodLink" : "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" ? "https://quickmark-1.onrender.com" : "http://localhost:5173",
     credentials: true,
 }));
 connectDB();
