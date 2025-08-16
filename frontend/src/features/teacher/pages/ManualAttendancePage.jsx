dd1bbbf3494e3ce649f6bfb3d00a08dbc7d1917a
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-16 08:52:59 +0530

    New page added for taking attendance manually

---
+++ frontend/src/features/teacher/pages/ManualAttendancePage.jsx
@@ -0,0 +1,11 @@
+import ManualAttendance from "../components/ManualAttendance";
+
+function ManualAttendancePage() {
+    return (
+        <>
+            <ManualAttendance />
+        </>
+    );
+}
+
+export default ManualAttendancePage;
