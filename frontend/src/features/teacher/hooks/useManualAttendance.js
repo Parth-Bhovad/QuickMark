d679d3e360a092c3727a009a0e5b245ef8fa394d
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-16 08:52:04 +0530

    custom hook added for Manual Attendance page

---
+++ frontend/src/features/teacher/hooks/useManualAttendance.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+//importing APIs
+import { addManualAttendanceAPI } from "../api/attendance.api";
+
+function useManualAttendance() {
+  const [rollNo, setRollNo] = useState("");
+  const [addingManualAttendance, setAddingManualAttendance] = useState(false);
+  const [feedback, setFeedback] = useState("");
+
+  const handleSubmit = async (e, selectedSubject) => {
+    e.preventDefault();
+    if (!rollNo.trim() || !selectedSubject) return;
+
+    try {
+      setAddingManualAttendance(true);
+      setFeedback("");
+      let res = await addManualAttendanceAPI(rollNo, selectedSubject, true);
+      console.log(res);
+
+      setRollNo("");
+    } catch (err) {
+      console.error("Error adding attendance:", err);
+      setFeedback("Failed to add attendance. " + (err.response.data.message || ""));
+    } finally {
+      setAddingManualAttendance(false);
+    }
+  };
+
+  return { rollNo, setRollNo, addingManualAttendance, handleSubmit, feedback };
+}
+
+export default useManualAttendance;
