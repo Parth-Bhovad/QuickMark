60d7664c2e0a6cd6f5d4ebb649e80e7aa087d33e
Parth Bhovad <parthbhovad710@gmail.com>
2025-08-16 08:50:06 +0530

    addManualAttendanceAPI added

--- frontend/src/features/teacher/api/attendance.api.js
+++ frontend/src/features/teacher/api/attendance.api.js
@@ -4,3 +4,12 @@ export const getAttendanceDataAPI = async (subjectName) => {
     const response = await api.get(`/attendance/${subjectName}`);
     return response;
 }
+
+export const addManualAttendanceAPI = async (rollNo, subjectName, isPresent) => {
+    const response = await api.post(`/attendance/by-teacher`, {
+        rollNo,
+        subjectName,
+        isPresent
+    });
+    return response;
+};
