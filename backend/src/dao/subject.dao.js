0d40f5eb0570553cf2558aee688c29c8e001194e
Parth Bhovad <parthbhovad710@gmail.com>
2025-10-25 16:18:36 +0530

    removed returning something when it is not needed for client

--- backend/src/dao/subject.dao.js
+++ backend/src/dao/subject.dao.js
@@ -4,7 +4,6 @@ import Subject from '../models/Subject.model.js';
 export const createSubject = async (subjectName, teacherId) => {
     const newSubject = new Subject({ subjectName, teacherId });
     await newSubject.save();
-    return newSubject;
 }
 
 export const findSubjectByName = async (subjectName) => {
