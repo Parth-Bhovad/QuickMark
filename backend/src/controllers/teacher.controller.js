f824f74573f9b10218610b1cfa6caba8a4d532c2
Parth Bhovad <parthbhovad710@gmail.com>
2025-10-25 16:17:54 +0530

    remove text msgs from res for successful responses

--- backend/src/controllers/teacher.controller.js
+++ backend/src/controllers/teacher.controller.js
@@ -11,15 +11,15 @@ export const getTeacher = async (req, res) => {
 
 export const deleteTeacher = async (req, res) => {
     let teacherId = req.params.teacherId;
-    const deletedTeacher = await deleteTeacherService(teacherId);
-    res.status(200).json({ teacher: deletedTeacher });
+    await deleteTeacherService(teacherId);
+    res.sendStatus(200);
 };
 
 export const addSubjectToTeacher = async (req, res) => {
         const { subjectName } = req.body;
         const teacherId = req.params.teacherId;
         await addSubjectToTeacherService(subjectName, teacherId);
-        res.status(200).json({ message: "Subject added to teacher successfully" });
+        res.sendStatus(200);
 }
 
 export const getTeacherSubjects = async (req, res) => {
@@ -30,6 +30,6 @@ export const getTeacherSubjects = async (req, res) => {
 
 export const teacherForgotPassword = async (req, res) => {
     let { teacherEmail, newPassword } = req.body;
-    const result = await teacherForgotPasswordService(teacherEmail, newPassword);
-    res.status(200).json(result);
+    await teacherForgotPasswordService(teacherEmail, newPassword);
+    res.sendStatus(200);
 }
